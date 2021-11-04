96f104d91ef7b9941c9248007d4e7631e0d3d48f
manoj kota <kota.manoj8@gmail.com>
2021-11-03 19:02:52 -0500

    valley added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var treeRouter = require('./routes/valley');
+
 
 
 var app = express();
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/valley',treeRouter);
 
 
 
--- routes/valley.js
+++ routes/valley.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("valley", { title: "Search Results valley" });
+  res.render("tree", { title: "Search Results Tree" });
 });
 
 module.exports = router;
