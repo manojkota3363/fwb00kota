b06908ee2ea8a2bd025f2a657069775a70ccd4cd
manoj kota <kota.manoj8@gmail.com>
2021-11-03 19:14:13 -0500

    addmods added

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var valleyRouter = require('./routes/valley');
+var addmodsRouter = require('./addmods/valley');
+
 
 
 
@@ -25,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/valley', valleyRouter);
+
+app.use('/addmods',addmodsRouter);
+
 
 
 
